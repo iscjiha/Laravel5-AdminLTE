3ebca6f2a3fba9f618bc14620fc1673dfb989e23
JIHA <isc.jiha@gmail.com>
2015-03-28 13:40:57 -0600

    Llamando archivos CSS en head desde carpeta public de Laravel
    Se modifico el llamado a algunos archivos CSS en head de forma como lo
    pide Laravel referenciando los archivos que agregamos a la carpeta public

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,28 +5,28 @@
 	<title>AdminLTE 2 | Dashboard</title>
 	<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 	<!-- Bootstrap 3.3.2 -->
-	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="{{ asset('/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
 	<!-- FontAwesome 4.3.0 -->
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
 	<!-- Ionicons 2.0.0 -->
 	<link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
 	<!-- Theme style -->
-	<link href="dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+	<link href="{{ asset('/dist/css/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
 	<!-- AdminLTE Skins. Choose a skin from the css/skins
          folder instead of downloading all of them to reduce the load. -->
-	<link href="dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
+	<link href="{{ asset('/dist/css/skins/_all-skins.min.css') }}" rel="stylesheet" type="text/css" />
 	<!-- iCheck -->
-	<link href="plugins/iCheck/flat/blue.css" rel="stylesheet" type="text/css" />
+	<link href="{{ asset('/plugins/iCheck/flat/blue.css') }}" rel="stylesheet" type="text/css" />
 	<!-- Morris chart -->
-	<link href="plugins/morris/morris.css" rel="stylesheet" type="text/css" />
+	<link href="{{ asset('/plugins/morris/morris.css') }}" rel="stylesheet" type="text/css" />
 	<!-- jvectormap -->
-	<link href="plugins/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+	<link href="{{ asset('/plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet" type="text/css" />
 	<!-- Date Picker -->
-	<link href="plugins/datepicker/datepicker3.css" rel="stylesheet" type="text/css" />
+	<link href="{{ asset('/plugins/datepicker/datepicker3.css') }}" rel="stylesheet" type="text/css" />
 	<!-- Daterange picker -->
-	<link href="plugins/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+	<link href="{{ asset('/plugins/daterangepicker/daterangepicker-bs3.css') }}" rel="stylesheet" type="text/css" />
 	<!-- bootstrap wysihtml5 - text editor -->
-	<link href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" />
+	<link href="{{ asset('/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}" rel="stylesheet" type="text/css" />
 
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
